332c28ce06aa0e2c85e829f4cd36ac17bce567bc
Jouni Koivuviita <jouni@vaadin.com>
2021-04-16 16:57:12 +0300

    add material theme

--- theme/material/vaadin-avatar-styles.js
+++ theme/material/vaadin-avatar-styles.js
@@ -26,6 +26,42 @@ registerStyles(
       border-color: var(--material-primary-color);
     }
 
+    :host([clickable]) {
+      cursor: pointer;
+      position: relative;
+    }
+
+    :host([clickable])::after {
+      content: '';
+      position: absolute;
+      z-index: 2;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      border-radius: inherit;
+      transition: opacity 0.2s;
+      opacity: 0;
+      background-color: var(--material-primary-color);
+    }
+
+    :host([clickable]:hover)::after {
+      opacity: 0.1;
+    }
+
+    :host([clickable]:active)::after {
+      opacity: 0.2;
+      transition-duration: 0s;
+    }
+
+    :host([has-color-index][clickable]:hover)::after {
+      opacity: 0.15;
+    }
+
+    :host([has-color-index][clickable]:active)::after {
+      opacity: 0.3;
+    }
+
     [part='icon'],
     [part='abbr'] {
       fill: currentColor;
